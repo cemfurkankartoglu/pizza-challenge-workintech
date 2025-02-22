12cacc71ee307c6f8804bf8bd7eb716fb1519cc7
cemfurkankartoglu <cemfurkankartoglu@gmail.com>
2025-02-22 13:49:43 +0300

    hazır kurulu olan css dosyalari sifirlandi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,14 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Header.css"
-import {
-  Navbar,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap";
 
 export default function Header(props) {
   return (
-    <div>
-      <Navbar className="navbar">
-        <Nav className="navbar-nav me-auto" navbar-nav>
-          <NavItem><img src="../../images/iteration-1-images/logo.svg" alt="Teknolojik Yemekler Logo" /></NavItem>
-          <NavItem>
-            <NavLink href="/">Anasayfa-</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/">
-              Seçenekler-
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/">
-              Sipariş Oluştur
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </div >
+    <header>
+      <div className="logo"><img src="../../images/iteration-1-images/logo.svg" alt="Teknolojik Yemekler Logo" /></div>
+      <nav>
+        <a href="/">Anasayfa</a><span className="duz-cizgi">-</span>
+        <a href="/">Seçenekler</a><span className="duz-cizgi">-</span>
+        <a href="/">Sipariş Oluştur</a>
+      </nav>
+    </header >
   )
 }
