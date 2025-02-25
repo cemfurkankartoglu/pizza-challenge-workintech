0e026228382a1a1afe66aa4da71df4dd4542de20
cemfurkan <cemfurkankartoglu@gmail.com>
2025-02-25 16:20:11 +0300

    IT1 testler ve form özellikleri hariç tamamlandı

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <main className="home-container">
-            <h1 className="home-baslik">KOD ACIKTIRIR PİZZA DOYURUR</h1>
+            <h1 className="home-baslik">KOD ACIKTIRIR <br /> PİZZA, DOYURUR</h1>
             <Link to="/order">
                 <button aria-label="Sipariş sayfasına git" className="btn-aciktim">ACIKTIM</button>
             </Link>
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,7 +1,7 @@
 export default function Success() {
     return (
         <main className="success-container">
-            <h1 className="success-baslik">TEBRİKLER! SİPARİŞİNİZ ALINDI</h1>
+            <h1 className="success-baslik">TEBRİKLER!<br />SİPARİŞİNİZ ALINDI</h1>
         </main>
     )
 }
